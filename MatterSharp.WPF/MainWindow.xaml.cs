8be6308edb4714fcd37ba67f5e176595f6f876a2
Andrey Andreev <aandreev@game-forest.com>
2017-01-17 11:43:37 +1000

    Add quick'n'dirty test for local Mattermost instance.

---
+++ MatterSharp.WPF/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatterSharp.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Uri uri = new Uri("");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Browser.Navigated += BrowserOnNavigated;
+            Browser.Navigate(uri);
+        }
+
+        private void BrowserOnNavigated(object sender, NavigationEventArgs navigationEventArgs)
+        {
+            var container = GetUriCookieContainer(uri);
+            var cookie = container.GetCookies(uri);
+            var token = cookie["MMAUTHTOKEN"].Value;
+            MainAction(token);
+            Close();
+        }
+
+        private void MainAction(string token)
+        {
+            var client = new MattermostClient(uri.AbsoluteUri, token);
+
+        }
+
+        [DllImport("wininet.dll", SetLastError = true)]
+        public static extern bool InternetGetCookieEx(
+       string url,
+       string cookieName,
+       StringBuilder cookieData,
+       ref int size,
+       Int32 dwFlags,
+       IntPtr lpReserved);
+
+        private const Int32 InternetCookieHttponly = 0x2000;
+
+        /// <summary>
+        /// Gets the URI cookie container.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <returns></returns>
+        public static CookieContainer GetUriCookieContainer(Uri uri)
+        {
+            CookieContainer cookies = null;
+            // Determine the size of the cookie
+            int datasize = 8192 * 16;
+            StringBuilder cookieData = new StringBuilder(datasize);
+            if (!InternetGetCookieEx(uri.ToString(), null, cookieData, ref datasize, InternetCookieHttponly, IntPtr.Zero))
+            {
+                if (datasize < 0)
+                    return null;
+                // Allocate stringbuilder large enough to hold the cookie
+                cookieData = new StringBuilder(datasize);
+                if (!InternetGetCookieEx(
+                    uri.ToString(),
+                    null, cookieData,
+                    ref datasize,
+                    InternetCookieHttponly,
+                    IntPtr.Zero))
+                    return null;
+            }
+            if (cookieData.Length > 0)
+            {
+                cookies = new CookieContainer();
+                cookies.SetCookies(uri, cookieData.ToString().Replace(';', ','));
+            }
+            return cookies;
+        }
+    }
+}
---
+++ MatterSharp/MattermostClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using RestSharp;
+
+namespace MatterSharp
+{
+    public class MattermostClient
+    {
+        private readonly RestClient restClient;
+
+        public MattermostClient(string url, string token)
+        {
+            var baseUrl = $"{url}api/v3";
+            restClient = new RestClient(baseUrl);
+            var request = new RestRequest("users/me", Method.GET);
+            request.AddParameter("Authorization", $"Bearer {token}", ParameterType.HttpHeader);
+            var result = restClient.Execute(request);
+            var a = 1;
+        }
+    }
+}
